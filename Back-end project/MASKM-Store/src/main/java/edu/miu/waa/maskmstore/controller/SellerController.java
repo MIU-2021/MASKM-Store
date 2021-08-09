204efc4029545545c48aa38b68614ca7bbf27c92
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2021-08-09 18:31:12 -0500

    k-Controllers-1.1

--- Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/controller/SellerController.java
+++ Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/controller/SellerController.java
@@ -1,19 +1,15 @@
 package edu.miu.waa.maskmstore.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin()
 @RestController
-@RequestMapping("/admin")
-public class AdminController {
+@RequestMapping("/seller")
+public class SellerController {
 
     @GetMapping
     public String getFirst(){
-        return "HelloWorld MASKM Admin Page";
+        return "HelloWorld MASKM Seller Page";
     }
-
-
 }
