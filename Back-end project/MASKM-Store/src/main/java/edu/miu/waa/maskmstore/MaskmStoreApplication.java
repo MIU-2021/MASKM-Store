5529f56e283ac85dddd911b2874517662498a09f
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2021-08-09 16:29:04 -0500

    Create-Backend-Project

---
+++ Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/MaskmStoreApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.waa.maskmstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskmStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaskmStoreApplication.class, args);
+    }
+
+}
---
+++ Back-end project/MASKM-Store/src/test/java/edu/miu/waa/maskmstore/MaskmStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.waa.maskmstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskmStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
