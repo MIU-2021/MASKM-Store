16cada7c66e87350c73fd03a98cd2eea8c16e0d5
ahmedelghannam1 <aelghannam@miu.edu>
2021-08-09 17:07:36 -0500

    create packages-1

--- Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/domain/User.java
+++ Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/domain/User.java
@@ -1,4 +1,17 @@
 package edu.miu.waa.maskmstore.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
 }
