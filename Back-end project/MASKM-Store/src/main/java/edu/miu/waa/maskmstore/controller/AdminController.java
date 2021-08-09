36c3ea581801d69b25273550e89269eabee3ac4c
Kareem AbuRijela <39139830+KareemAbuRejila@users.noreply.github.com>
2021-08-09 17:34:22 -0500

    k-Admin-Controller-2

---
+++ Back-end project/MASKM-Store/src/main/java/edu/miu/waa/maskmstore/controller/AdminController.java
@@ -0,0 +1,15 @@
+package edu.miu.waa.maskmstore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String getFirst(){
+        return "HelloWorld MASKM Admin Page";
+    }
+}
